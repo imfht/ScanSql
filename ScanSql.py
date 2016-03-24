6d7328fc5126bb7d2e9d86f058ef9dfd0fb48d5a
fiht <fiht@qq.com>
2016-03-24 23:20:33 +0800

    改正了一些bug

--- ScanSql.py
+++ ScanSql.py
@@ -14,26 +14,36 @@ class WebSite():
 class BigFuck():
     def __init__(self,url):
         self.url = url
+        self.friend_url = []
+        self.payload_url = []
     def get_things(self):
         req = requests.get(self.url)
         soup = BeautifulSoup(req.text,'lxml')
         #print(req.text)
         pattern = re.compile('href="(.*?)"')
         for i in re.findall(pattern,req.text):
-            #print(i)
-            if i.find('=')!=-1:
-                if i.find('http'):
-                    i = self.url+i
+            if i.find('http')!=-1:#有http->友情链接
+                #webSiteSet.add(i)
+                #print('友情链接',i) 
+                #----------------------------------------------------------------------------#
+                #友情链接还得处理 ->urlprase 看里面是否有注入点 如果有，去重扫描，并将域名丢到友情链接中等待扫描
+                #----------------------------------------------------------------------------#
+                self.friend_url.append(i)
+            elif i.find('=')!=-1: #无参数
+#                    i = self.url+i #带参数的链接
+                self.payload_url.append(self.url+i)
+                    #print('带参数的链接'+self.url+i)
+            else:
+                #print('没用的url',i) #不带参数，无http关键词的链接
+                pass
                 
-            #if i['href'].find('http')!=-1:
-                #webSiteSet.add(i['href'])
-            #else:
-                #print(i['href'])
-            #存在href中存在http标记的即为友情链接
-            #不存在href的即为非友情链接（是否存在url参数以检索payload）
-        #print(req.text)
-        #for i in webSiteSet:
-            #print(i)
+            #逻辑：href带http->友链
+            #href不带http->有参数(=)->可能是payload，丢进去去重
+            #href不带http->且无参数->无用url 直接丢弃
+        for i in self.friend_url:
+            print('友链',i)
+        for i in self.payload_url:
+            print('可能存在payload的链接',i)
 def parse_url():
     fuck_set = set()
     url = ['http://www.hnfnu.edu.cn/NewsList.aspx?bbid=110&nid=10829','http://www.hnfnu.edu.cn/NewsList.aspx?bbid=111&nid=10835']
@@ -43,6 +53,5 @@ def parse_url():
             #print(i.split('=')[0])
             fuck_set.add(i.split('=')[0])
     print(fuck_set)
-#a = BigFuck('http://www.hnfnu.edu.cn/')
-#a.get_things()
-parse_url()
+a = BigFuck('http://www.hnfnu.edu.cn/')
+a.get_things()
