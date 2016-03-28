25f47fc455ae03a8fb913a05c2f679f621d44b18
fiht <fiht@qq.com>
2016-03-28 16:35:05 +0800

    脚本

--- sh.sh
+++ sh.sh
@@ -3,7 +3,6 @@ count=0
 echo "请将本文件放在sqlmap目录下 并将要扫描的文件命名为all_webSite\n注意大小写哦么么哒"
 echo "如果你不知道该折腾哪些网站你可以考虑使用getWebSite脚本 利用管道重定向到all_webSite中"
 echo "enjoy!"
-echo "我要开始扫描了哦～可能会让你你看起来我已经假死了"
 for i in $(cat all_webSite)
 do
         for ii in $(python3 ./ScanSql.py 'http://'$i:)
